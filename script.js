2ba758552edb99f41f19c2bdc87a521b15fff01c
SOICHIRO HATSUKAIWA <49380301+Soichi0809@users.noreply.github.com>
2024-01-03 23:18:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ resetbtn.onclick = function() {
 
 var group_num;
 
+var alreadysbumit
+if(localStorage.getItem("alreadysbumit") == null){
+    alreadysbumit = false;
+}
+
 function getgroupnum(){
     group_num = localStorage.getItem("group_num");
     var url = location.href;
@@ -21,7 +26,7 @@ function getgroupnum(){
     return group_num;
 }
 
-var answered = [0,0,0,0,0,0,0]
+var answered = [0,0,0,0,0,0,0,0]
 
 window.onload = function() {
     //answered をjsonから取得
@@ -34,7 +39,7 @@ window.onload = function() {
 }
 const api_url = "https://script.google.com/macros/s/AKfycbwwNAqCvrzX1hcq9QLoZeBkrWo3tgG72oPAG8K1zdUDL6VE_mQCtZgNuKjNW107tEdi/exec";
 
-var answer = ["1", "2", "3", "4", "5", "6", "7"];
+var answer = ["1", "2", "3", "4", "5", "6", "7" , "last"];
 
 // モーダル要素を取得
 var modal = document.getElementById("myModal");
@@ -119,7 +124,11 @@ submitBtn.onclick = function() {
         storymodal.classList.add("show");
         correct_telling.style.display = "block";
         storymodal.scrollTop = 0;
+        if(answered[0] == 1 && answered[1] == 1 && answered[2] == 1 && answered[3] == 1 && answered[4] == 1 && answered[5] == 1 && answered[6] == 1 && answered[7] == 1) {
+            submit_lastans();
+        }
     }
+    
     document.getElementById("passwordInput").value = ""; // 入力欄をリセット
 }
 
@@ -129,12 +138,12 @@ function update(anss){
             ansbtns[index].classList.add("button-correct");
             ansbtns[index].classList.remove("button-default");
             ansbtns[index].disabled = true;
-            storybtns[index].classList.add("button-default");
-            storybtns[index].disabled = false;
             menubox[index].classList.add("menu-box-correct");
             menubox[index].classList.remove("menu-box");
+            storybtns[index].classList.add("button-default");
+            storybtns[index].disabled = false;
             answered[index] = 1;
-            if(answered[0] == 1 && answered[1] == 1 && answered[2] == 1) {
+            if(answered[0] == 1 && answered[1] == 1 && answered[2] == 1 && answered[3] == 1 && answered[4] == 1 && answered[5] == 1 && answered[6] == 1 && answered[7] != 1) {
                 lastbtn.disabled = false;
             }
             //answered をjsonに変換してlocalStorageに保存
@@ -144,23 +153,26 @@ function update(anss){
     
 }
 
-lastbtn.onclick = function() {
-    fetch(api_url, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: encodeURI(`group_num=${getgroupnum()}`)
-    })
-        .then((response) => {
-            response.text().then((text) => {
-                alert(text);
-            });
+function submit_lastans() {
+    if(alreadysbumit == false){
+        fetch(api_url, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: encodeURI(`group_num=${getgroupnum()}`)
         })
-        .catch((error) => {
-            alert(error.message);
-        });
-    
+            .then((response) => {
+                response.text().then((text) => {
+                    alert(text);
+                });
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
+        alreadysbumit = true;
+        localStorage.setItem("alreadysbumit", alreadysbumit);
+    }
 }
 var story = ["なぞなぞ（謎謎、なぞ）は、問いかけに対して、とんちを利かせた答えを要求する言葉遊びを用いたクイズである。ただし普通のクイズとは違って正解は事実に基づくものではなく、言葉の意味をこじつけた駄洒落・洒落が多い。韻を踏んでいたり、何かに見立てられたりする[1]。転じて、言葉によって婉曲的にわからせる事についてもなぞなぞという。",
 "クイズ（Quiz）の英語での意味は、「（何か）質問すること」と 「知識をテストすること」 と、これらの名詞としての意味であり、日本語では後者の「知識を問う問題」の意味で使われている。出題者が既知の事実に対して質問をし、解答者がその質問に対する正解を答えるという遊び。あるいはその質問のこと。",
@@ -168,4 +180,5 @@ var story = ["なぞなぞ（謎謎、なぞ）は、問いかけに対して、
 "4",
 "5",
 "6",
-"7"];
+"7",
+"last answer is correct"];
