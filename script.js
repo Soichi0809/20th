2ca953868714e6b49d1b387d23e9e0cd1fe49fe2
SOICHIRO HATSUKAIWA <49380301+Soichi0809@users.noreply.github.com>
2024-01-04 05:23:27 +0900

    Add files via upload
    huge change

--- script.js
+++ script.js
@@ -39,16 +39,12 @@ window.onload = function() {
 }
 const api_url = "https://script.google.com/macros/s/AKfycbwwNAqCvrzX1hcq9QLoZeBkrWo3tgG72oPAG8K1zdUDL6VE_mQCtZgNuKjNW107tEdi/exec";
 
-var answer = ["1", "2", "3", "4", "5", "6", "7" , "last"];
 
 // モーダル要素を取得
 var modal = document.getElementById("myModal");
 
 // ストーリーモーダル要素を取得
 var storymodal = document.getElementById("storyModal");
-
-// 最終解答用ボタン要素を取得
-var lastbtn = document.getElementById("lastbutton");
 
 // 「解答する」ボタン要素を取得
 var ansbtns = document.querySelectorAll('.openAnswer')
@@ -62,25 +58,29 @@ var span = document.getElementsByClassName("close");
 // ボックス要素を取得
 var menubox = document.querySelectorAll(".menu-box");
 
-// 「解答する」ボタンを押下したときのイベントリスナーを設定
-ansbtns.forEach(function(btn, index) {
+// 「解答する」「ストーリーを見る」ボタン要素を取得
+var boxmsg = document.querySelectorAll(".box-content");
+
+var btnbox = document.querySelectorAll(".button-box");
+
+// boxをクリックしたときのイベントリスナーを設定
+menubox.forEach(function(btn, index) {
     // 各要素にクリックイベントリスナを追加
-    btn.addEventListener('click', function() {
-        modal.classList.add("show");
-        now_index = index;
-    });
+    if(index < 7){
+        btn.addEventListener('click', function() {
+            if(answered[index] == 0){
+                modal.classList.add("show");
+                now_index = index;
+            }else{
+                storymodal.classList.add("show");
+                storymodal.scrollTop = 0;
+                now_index = index;
+                document.getElementById("storybox").innerHTML = story[now_index];
+            }
+        });
+    }
 });
 
-// 「ストーリー」ボタンを押下したときのイベントリスナーを設定
-storybtns.forEach(function(btn, index) {
-    // 各要素にクリックイベントリスナを追加
-    btn.addEventListener('click', function() {
-        storymodal.classList.add("show");
-        storymodal.scrollTop = 0;
-        now_index = index;
-        document.getElementById("storybox").innerHTML = story[now_index];
-    });
-});
 
 
 // ユーザーがxをクリックするとモーダルを閉じる
@@ -120,7 +120,7 @@ submitBtn.onclick = function() {
     if(password == answer[now_index]) {
         document.getElementById("storybox").innerHTML = story[now_index];
         answered[now_index] = 1;
-        update(answered);
+        update(answered);storyModal
         storymodal.classList.add("show");
         correct_telling.style.display = "block";
         storymodal.scrollTop = 0;
@@ -135,16 +135,30 @@ submitBtn.onclick = function() {
 function update(anss){
     anss.forEach(function(ans, index) {
         if(ans){
-            ansbtns[index].classList.add("button-correct");
-            ansbtns[index].classList.remove("button-default");
-            ansbtns[index].disabled = true;
             menubox[index].classList.add("menu-box-correct");
             menubox[index].classList.remove("menu-box");
-            storybtns[index].classList.add("button-default");
-            storybtns[index].disabled = false;
+            boxmsg[index].innerHTML = "ストーリーを見る";
+            var img = document.createElement("img");
+            img.src = "sumi.svg";
+            img.classList.add("sumi");
+            img.width = 100;
+            boxmsg[index].appendChild(img);
+            boxmsg[index].classList.add("box-content-correct");
+            btnbox[index].classList.remove("button-box-before");
             answered[index] = 1;
             if(answered[0] == 1 && answered[1] == 1 && answered[2] == 1 && answered[3] == 1 && answered[4] == 1 && answered[5] == 1 && answered[6] == 1 && answered[7] != 1) {
-                lastbtn.disabled = false;
+                btnbox[7].classList.add("button-box-before");
+                menubox[7].addEventListener('click', function() {
+                    if(answered[7] == 0){
+                        modal.classList.add("show");
+                        now_index = 7;
+                    }else{
+                        storymodal.classList.add("show");
+                        storymodal.scrollTop = 0;
+                        now_index = 7;
+                        document.getElementById("storybox").innerHTML = story[now_index];
+                    }
+                });
             }
             //answered をjsonに変換してlocalStorageに保存
             localStorage.setItem("answered", JSON.stringify(answered));
@@ -174,6 +188,11 @@ function submit_lastans() {
         localStorage.setItem("alreadysbumit", alreadysbumit);
     }
 }
+
+var answer = ["1", "2", "3", "4", "5", "6", "7" , "last"];
+/* 本番用
+var answer = ["えいごか", "いくほうかん", "たてしな", "きょういく", "とうきょうどーむ", "そうがんきょう", "むしとりあみ" , "last"];
+*/
 var story = ["なぞなぞ（謎謎、なぞ）は、問いかけに対して、とんちを利かせた答えを要求する言葉遊びを用いたクイズである。ただし普通のクイズとは違って正解は事実に基づくものではなく、言葉の意味をこじつけた駄洒落・洒落が多い。韻を踏んでいたり、何かに見立てられたりする[1]。転じて、言葉によって婉曲的にわからせる事についてもなぞなぞという。",
 "クイズ（Quiz）の英語での意味は、「（何か）質問すること」と 「知識をテストすること」 と、これらの名詞としての意味であり、日本語では後者の「知識を問う問題」の意味で使われている。出題者が既知の事実に対して質問をし、解答者がその質問に対する正解を答えるという遊び。あるいはその質問のこと。",
 "パズル（英語：Puzzle）は、あらかじめ出された問題を、論理的な考察と試行錯誤によって解くことを目的とした、ゲームやクイズに似た娯楽の一種。",
